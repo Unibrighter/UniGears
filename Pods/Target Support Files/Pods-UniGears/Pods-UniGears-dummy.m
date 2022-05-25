4f0107a08d2a5ce50d2ec36e4fcce31459e3a5da
Robert Wu <rocklee.au@foxmail.com>
2022-05-25 21:11:28 +1000

    Empty project Added

---
+++ Pods/Target Support Files/Pods-UniGears/Pods-UniGears-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_UniGears : NSObject
+@end
+@implementation PodsDummy_Pods_UniGears
+@end
---
+++ UniGears/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniGears
+//
+//  Created by Bitmad on 25/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
